8740ef8f15c365773392f99ca81b21e817c65d2e
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-17 01:29:13 +0000

    Add question

--- Node/app.js
+++ Node/app.js
@@ -1,8 +1,11 @@
 require('./instantHello');
 var goodbye = require('./talk/goodbye');
 var talk = require('./talk');
+var question = require('./talk/question');
 
 talk.intro();
 talk.hello("Simon");
 
+var answer =question.ask("What is the meaning of life?");
+    console.log(answer);
 goodbye();
