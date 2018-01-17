9ffa4b4a8208c3e9c28e6b8f78bd403afd26dc4a
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-17 02:39:03 +0000

    Add onFileLoad

--- Node/readFileAsync.js
+++ Node/readFileAsync.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 
+var onFileLoad = function(err, file) {
 console.log("Going to get a file");
-fs.readFile('readFileSync.js', function(err, file) {
+};
+fs.readFile('readFileSync.js', onFileLoad);
 console.log("Got the file");
-});
+
 console.log("App continues...");
