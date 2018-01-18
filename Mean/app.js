bf387dd37aec182e863e2741497f6ac6e3e09fbe
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-18 03:34:02 +0000

    Add GET

--- Mean/app.js
+++ Mean/app.js
@@ -3,6 +3,20 @@ var app = express();
 
 app.set('port', 3000);
 
+app.get('/', function(req, res) {
+    console.log("GET the homepage");
+    res
+    .status(404)
+    .send("Express yourself");
+});
+
+app.get('/json', function(req, res) {
+    console.log("GET the json");
+    res
+    .status(200)
+    .json( {"jsonData" : true } );
+});
+
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
 console.log('Magic happens on port ' + port);
