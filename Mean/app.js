dfaf4f0c66fb24f6048d49dc0718d2e53c03d545
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-18 02:34:48 +0000

    Add npm start

--- Mean/app.js
+++ Mean/app.js
@@ -1,11 +1,1 @@
-require('./instantHello');
-var goodbye = require('./talk/goodbye');
-var talk = require('./talk');
-var question = require('./talk/question');
-
-talk.intro();
-talk.hello("Simon");
-
-var answer =question.ask("What is the meaning of life?");
-    console.log(answer);
-goodbye();
+console.log("it works!");
