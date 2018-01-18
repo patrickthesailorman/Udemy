f811d63d06a5a1ddbc49fd26bed4519511a5d6ab
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-18 02:48:46 +0000

    Add app.js

--- Mean/app.js
+++ Mean/app.js
@@ -1,1 +1,11 @@
-console.log("it works!");
+var express = require('express');
+var app = express();
+
+app.set('port', 3000);
+
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+console.log('Magic happens on port ' + port);
+ });
+ 
+ console.log('Me first!');
