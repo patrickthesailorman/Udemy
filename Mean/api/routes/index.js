e8190e6689cc331a573fc72855a59b3e9d23158c
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-20 04:05:56 +0000

    Add controllers directory

--- Mean/api/routes/index.js
+++ Mean/api/routes/index.js
@@ -2,19 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router
-    .route('/json')
+    .route('/hotels')
     .get('/json', function(req, res) {
     console.log("GET the json");
     res
       .status(200)
       .json( {"jsonData" : true} );
-})
-    .post('/json', function(req, res) {
-    console.log("POST the json route");
-    res
-      .status(200)
-      .json( {"jsonData" : "POST recieved"} );
 });
+
     
 
 module.exports = router;
