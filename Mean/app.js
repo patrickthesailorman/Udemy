2e72a27b474e4c3592698a770a84e9cd230e740c
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-20 03:50:57 +0000

    Add routes

--- Mean/app.js
+++ Mean/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.set('port', process.env.PORT);
 
-app.use('/css', function(req, res, next) {
+app.use(function(req, res, next) {
    console.log(req.method, req.url);
    next();
 });
