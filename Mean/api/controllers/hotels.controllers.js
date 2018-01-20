9a968e2cff47acbef9ed7a5c5dfb31121ff4196a
Patrick McCarty <patrickthesailorman@gmail.com>
2018-01-20 05:05:46 +0000

    Add Query

--- Mean/api/controllers/hotels.controllers.js
+++ Mean/api/controllers/hotels.controllers.js
@@ -2,9 +2,24 @@ var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
     console.log("GET the hotels");
+    console.log(req.query);
+    
+    var offset = 0;
+    var count = 5;
+    
+    if(req.query && req.query.offset) {
+        offset = parseInt(req.query.offset, 10);
+    }
+    
+     if(req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
+    }
+    
+    var returnData = hotelData.slice(offset, offset + count);
+    
     res
     .status(200)
-    .json( hotelData );
+    .json( returnData );
 };
 
 module.exports.hotelsGetOne = function(req, res) {
