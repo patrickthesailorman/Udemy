b569d0cdbcfa2a924bc1582fe7909b0c8182468f
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-05 02:33:11 +0000

    Add new post

--- Mean/api/controllers/hotels.controllers.js
+++ Mean/api/controllers/hotels.controllers.js
@@ -30,3 +30,11 @@ module.exports.hotelsGetOne = function(req, res) {
     .status(200)
     .json( thisHotel );
 };
+
+module.exports.hotelsAddOne = function(req, res) {
+    console.log("POST new hotel");
+    console.log(req.body);
+    res
+    .status(200)
+    .json(req.body);
+};
