515e23a2eecbf6ea554347b7d2fd397bd9316ff8
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-07 01:48:21 +0000

    Add dbconnection.js

---
+++ Mean/api/data/dbconnection.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+var dburl = 'mongodb://' + process.env.ip + ' :27017'
+
+var _connection = null;
+
+var open = function() {
+    MongoClient.connect(dburl, function (err, client) {
+        if(err) {
+          console.log("DB connection failed");
+          return;
+        }
+        _connection = client.db("meanhotel");
+        console.log("DB connection open", client);
+    });
+};
+var get = function() {
+    return _connection;
+};
+
+module.exports = {
+    open : open,
+    get : get
+};
+
+
+
