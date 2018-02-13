ef4a40336e6868649516ec29c5c55af50687abc2
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-13 00:53:14 +0000

    getOne

--- Mean/api/controllers/reviews.controllers.js
+++ Mean/api/controllers/reviews.controllers.js
@@ -34,3 +34,34 @@ module.exports.reviewsGetOne = function(req, res) {
               .json( review );
     });
 };
+
+module.exports.reviewsAddOne = function(req, res) {
+    var id = req.params.hotelId;
+    console.log("GET hotelId", id);
+    
+   Hotel
+      .findById(id)
+      .select('reviews')
+      .exec(function(err, doc) { 
+                    var response = {
+              status : 200,
+              message : doc
+          };
+       if (err) {
+          console.log("Error finding Hotel");
+          response.status = 500,
+          response.message = err;
+        } else if(!doc) {
+            console.log("Hotel id not found in database", id);
+          response.status = 404,
+          response.message = {
+            "message" : "Hotel ID not found" + id
+        };
+        } else {
+            response.message = doc.reviews ? doc.reviews : [];
+        }
+        res
+          .status(response.status)
+          .json(response.message);
+    });
+};
