c1b234661b892faa92da9f7be398b34f3b935eaa
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-13 21:57:02 +0000

    Add hotelUpdateOne

--- Mean/api/controllers/hotels.controllers.js
+++ Mean/api/controllers/hotels.controllers.js
@@ -95,11 +95,12 @@ module.exports.hotelsGetAll = function(req, res) {
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-    var id = req.params.hotelId;
-    console.log("GET hotelId", id);
+    var hotelId = req.params.hotelId;
+    console.log("GET hotelId", hotelId);
     
   Hotel
-    .findById(id)
+    .findById(hotelId)
+    .select("-reviews -rooms")
     .exec(function(err, doc) {
       var response = {
         status : 200,
@@ -110,15 +111,12 @@ module.exports.hotelsGetOne = function(req, res) {
         response.status = 500;
         response.message = err;
       } else if(!doc) {
-        console.log("HotelId not found in database", id);
+        console.log("HotelId not found in database");
         response.status = 404;
         response.message = {
-          "message" : "Hotel ID not found " + id
+          "message" : "Hotel ID not found "
         };
       }
-      res
-        .status(response.status)
-        .json(response.message);
     });
 
 };
@@ -163,4 +161,60 @@ module.exports.hotelsAddOne = function(req, res) {
         }
     });
 
+};
+
+module.exports.hotelsUpdateOne = function(req, res) {
+     var hotelId = req.params.hotelId;
+    console.log("GET hotelId", hotelId);
+    
+  Hotel
+    .findById(hotelId)
+    .exec(function(err, doc) {
+      var response = {
+        status : 200,
+        message : doc
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!doc) {
+        console.log("HotelId not found in database");
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found "
+        };
+      }
+      if (response.status !== 200) {
+      res
+        .status(response.status)
+        .json(response.message);
+      } else {
+        doc.name = req.body.name,
+        doc.description = req.body.description,
+        doc.stars = parseInt(req.body.stars, 10),
+        doc.services = _splitArray(req.body.services),
+        doc.photos = _splitArray(req.body.photos),
+        doc.currency = req.body.currency,
+        doc.location = {
+            address: req.body.address,
+            coordinates :[ 
+                parseFloat(req.body.lng), 
+                parseFloat(req.body.lat)
+                ]
+        };
+        
+        doc.save(function(err,hotelUpdated) {
+            if (err) {
+                res
+                .status(500)
+                .json(err);
+            } else {
+                res
+                .status(204)
+                .json();
+            }
+        });
+      }
+    });
 };
