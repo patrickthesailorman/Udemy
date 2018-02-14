6a43cba16a271f1b05cffb659be1b55366acb158
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-14 01:43:01 +0000

    Refactor reviewsGetAll

--- Mean/api/controllers/reviews.controllers.js
+++ Mean/api/controllers/reviews.controllers.js
@@ -3,19 +3,34 @@ var Hotel = mongoose.model('Hotel');
 
 // Get all reviews for a hotel
 module.exports.reviewsGetAll = function(req, res) {
-    var hotelId = req.params.hotelId;
-    console.log("GET hotelId", hotelId);
+    var id = req.params.hotelId;
+    console.log("GET hotelId", id);
     
-   Hotel
-      .findById(hotelId)
-      .select('reviews')
-      .exec(function(err, doc) { 
-          console.log("Returned doc", doc);
-        res
-          .status(200)
-          .json( doc.reviews );
+  Hotel
+    .findById(id)
+    .select('reviews')
+    .exec(function(err, doc) {
+      var response = {
+        status : 200,
+        message : []
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!doc) {
+        console.log("Hotel id not found in database", id);
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found " + id
+        };
+      } else {
+        response.message = doc.reviews ? doc.reviews : [];
+      }
+      res
+        .status(response.status)
+        .json(response.message);
     });
-    
 };
 //Get a single review for a hotel
 module.exports.reviewsGetOne = function(req, res) {
