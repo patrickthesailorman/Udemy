8749effcab0bd80ca5a74e6236036bf4df8ad03d
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-17 19:18:41 +0000

    Add RegisterController

--- Mean/public/angular-app/register/register-controller.js
+++ Mean/public/angular-app/register/register-controller.js
@@ -2,4 +2,27 @@ angular.module('meanhotel').controller('RegisterController', RegisterController)
 
 function RegisterController() {
     var vm = this;
+    
+    vm.register = function() {
+        var user = {
+            username: vm.username,
+            password: vm.password
+        };
+        
+        if(!vm.username || !vm.password) {
+            vm.error = 'Please enter a username and a password.'
+        } else {
+            if (vm.passord !== vm.passwordRepeat) {
+                vm.error = 'Pleas be sure the passwords match.'
+            } else {
+                $https.post('/api/users/register', user).then(function(result) {
+                    console.log(result);
+                    vm.message = 'Successful registration, please login.';
+                    vm.error = '';
+                }).catch(function(error) {
+                    console.log(error);
+                });
+            }
+        }
+    }
 };
