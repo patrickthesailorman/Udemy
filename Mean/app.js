f65e41972e5fbd6826728304cfcc078e5ffdf164
Patrick McCarty <patrickthesailorman@gmail.com>
2018-02-18 02:11:03 +0000

    update app.js

--- Mean/app.js
+++ Mean/app.js
@@ -6,35 +6,41 @@ var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
+// Define the port to run on
 app.set('port', process.env.PORT);
 
+// Add middleware to console log every request
 app.use(function(req, res, next) {
    console.log(req.method, req.url);
    next();
 });
 
+// Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+// Enable parsing of posted forms
 app.use(bodyParser.urlencoded( {extended : false}));
+app.use(bodyParser.json());
 
+// Add some routing
 app.use('/api', routes);
 
-app.get('/', function(req, res) {
-    console.log("GET the homepage");
-    res
-      .status(200)
-      .sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', function(req, res) {
+//     console.log("GET the homepage");
+//     res
+//       .status(200)
+//       .sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
+// app.get('/file', function(req, res) {
+//     console.log("GET the file");
+//     res
+//       .status(200)
+//       .sendFile(path.join(__dirname, 'app.js'));
+// });
 
-
-app.get('/file', function(req, res) {
-    console.log("GET the file");
-    res
-      .status(200)
-      .sendFile(path.join(__dirname, 'app.js'));
-});
-
+// Listen for requests
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
 console.log('Magic happens on port ' + port);
